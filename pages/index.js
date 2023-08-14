9e7360819a2ac7d6f656e98fa275778a3588728c
Daniel Arceno <108193380+Danielarceeno@users.noreply.github.com>
2023-08-14 19:49:07 +0000

    muda a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>O grande poeta, sabe que não é ou é</h1>
+    return <h1>Se vives de acordo com as leis da natureza, nunca serás pobre; se vives de acordo com as opiniões alheias, nunca serás rico</h1>
 }
 
 export default Home;
