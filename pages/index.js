7643a055ebc497cea1b91e0398ed9cc87c53f384
Daniel Arceno <108193380+Danielarceeno@users.noreply.github.com>
2023-08-20 18:36:10 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Se vives de acordo com as leis da natureza, nunca serás pobre; se vives de
-      acordo com as opiniões alheias, nunca serás rico
+      acordo com as opiniões alheias, nunca serás rico.
     </h1>
   );
 }
