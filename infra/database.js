135a3580f1e0e380e167a202bae457ed9895b451
Daniel Arceno <108193380+Danielarceeno@users.noreply.github.com>
2024-08-13 13:28:35 +0000

    add `add /api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,7 +1,21 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const cliente = new Client({
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
+async function getNewClient() {
+  const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
@@ -9,22 +23,14 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     ssl: getSSLValues(),
   });
- 
 
-  try {
-    await cliente.connect();
-    const result = await cliente.query(queryObject);
-    return result;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  } finally {
-    await cliente.end();
-  }
+  await client.connect();
+  return client;
 }
 
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
 
 function getSSLValues() {
